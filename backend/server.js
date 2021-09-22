0a13c3d5e44d22db54504b8e45ec926f16f1678f
nalindadsn <nalindadsn@gmail.com>
2021-09-22 11:57:43 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://nalindadsn:941151663Bandara@codeaddon.tmqu7.mongodb.net/teafactory?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true,
+  useCreateIndex: true,
 });
 
 app.get('/api/products/:id', (req, res) => {
@@ -18,19 +19,19 @@ app.get('/api/products/:id', (req, res) => {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
+
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
-
-app.use('/api/users', userRouter);
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
