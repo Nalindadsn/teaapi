7ba828fc867c45535cfb7f04af6d4dd2edf5fe38
nalindadsn <nalindadsn@gmail.com>
2021-09-22 11:46:12 +0530

    first commit

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -8,9 +8,10 @@ const userRouter = express.Router();
 userRouter.get(
   '/seed',
   expressAsyncHandler(async (req, res) => {
-     await User.remove({});
-    const createdUsers = await User.insertMany(data.users);
-    res.send({ createdUsers });
+    await User.remove({});
+    //console.log(data.users)
+     const createdUsers = await User.insertMany(data.users);
+     res.send({ createdUsers });
   })
 );
 
